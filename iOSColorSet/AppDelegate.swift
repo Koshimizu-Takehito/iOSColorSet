64f7fc252e4d8237e84253b74e232ac699e438b8
takehito koshimizu <takehito.koshimizu@gmail.com>
2019-01-20 18:06:02 +0900

    Initial Commit

---
+++ iOSColorSet/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  iOSColorSet
+//
+//  Created by Takehito Koshimizu on 2019/01/20.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ iOSColorSet/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iOSColorSet
+//
+//  Created by Takehito Koshimizu on 2019/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
